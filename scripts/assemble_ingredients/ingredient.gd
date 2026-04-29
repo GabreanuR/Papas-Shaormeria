0280b61ce1d1b2106ba6abe7c75351f443688dc2
Biancaaq <draghici.bianca1975@gmail.com>
2026-04-29 11:38:34 +0300

    Added new ingredients & fixed some bugs

--- scripts/assemble_ingredients/ingredient.gd
+++ scripts/assemble_ingredients/ingredient.gd
@@ -1,13 +1,25 @@
 extends TextureRect
 
 @export var nume_ingredient: String = "Cartofi"
+@export var textura_shaorma: Texture2D
 
 func _get_drag_data(at_position):
 	var preview = TextureRect.new()
-	preview.texture = texture
+	
+	if textura_shaorma:
+		preview.texture = textura_shaorma
+	else:
+		preview.texture = texture
+		
+	preview.expand_mode = TextureRect.EXPAND_KEEP_SIZE
+	
 	var control = Control.new()
 	control.add_child(preview)
-	preview.position = -texture.get_size() / 2
+	preview.position = -preview.texture.get_size() / 2
 	set_drag_preview(control)
 	
-	return {"nume": nume_ingredient, "poza": texture}
+	return {
+			"nume": nume_ingredient, 
+			"poza": texture, 
+			"poza_shaorma": textura_shaorma 
+	}
--- scripts/assemble_ingredients/lipie.gd
+++ scripts/assemble_ingredients/lipie.gd
@@ -13,19 +13,35 @@ func _drop_data(_at_position, data):
 	var rot_random = randf_range(0, 6.28)
 	
 	ingrediente_puse.append(data["nume"])
-	print("Shaorma actuala contine: ", ingrediente_puse)
+	
+	var textura_finala = data["poza"]
+	var este_mic = false
+	if data.has("poza_shaorma") and data["poza_shaorma"] != null:
+		textura_finala = data["poza_shaorma"]
+		este_mic = true
 	
 	var clona_lipie = Sprite2D.new()
-	clona_lipie.texture = data["poza"]
+	clona_lipie.texture = textura_finala
 	container_lipie.add_child(clona_lipie)
 	clona_lipie.position = container_lipie.to_local(mouse_pos)
 	clona_lipie.rotation = rot_random
 	
+	if este_mic:
+		if data["nume"] == "Chilli flakes":
+			clona_lipie.scale = Vector2(0.5, 0.5)
+		else:
+			clona_lipie.scale = Vector2(0.9, 0.9)
+	
 	if container_mizerie:
 		var rest_masa = Sprite2D.new()
-		rest_masa.texture = data["poza"]
+		rest_masa.texture = textura_finala
 		container_mizerie.add_child(rest_masa)
 		rest_masa.global_position = mouse_pos
 		rest_masa.rotation = rot_random
 		rest_masa.modulate = Color(0.7, 0.7, 0.7)
+		if este_mic:
+			if data["nume"] == "Chilli flakes":
+				rest_masa.scale = Vector2(0.5, 0.5)
+			else:
+				rest_masa.scale = Vector2(0.9, 0.9)
 		rest_masa.add_to_group("mizerie")
